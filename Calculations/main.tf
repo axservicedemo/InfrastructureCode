cf66d982b3b55ebada5123e16472bbd6b48d3728
axservicedemo <71280084+axservicedemo@users.noreply.github.com>
2020-09-16 20:05:43 +0530

    Added Storage

--- Calculations/main.tf
+++ Calculations/main.tf
@@ -69,6 +69,22 @@ resource "azurerm_windows_virtual_machine" "main" {
   }
 }
 
+resource "azurerm_managed_disk" "example" {
+  name                 = "${var.prefix}-disk1"
+  location             = azurerm_resource_group.main.location
+  resource_group_name  = azurerm_resource_group.main.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 5
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "example" {
+  managed_disk_id    = azurerm_managed_disk.example.id
+  virtual_machine_id = azurerm_windows_virtual_machine.main.id
+  lun                = "1"
+  caching            = "ReadWrite"
+}
+
 output "public_ip" {
   value = azurerm_windows_virtual_machine.main.public_ip_address
 }
