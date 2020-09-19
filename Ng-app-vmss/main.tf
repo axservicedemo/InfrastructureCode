860c1fb670108017b805558146d8e0ca0d9051c4
Ragavi <ragavi@appranix.com>
2020-09-19 16:19:03 +0530

    Added the image_id support

--- Ng-app-vmss/main.tf
+++ Ng-app-vmss/main.tf
@@ -6,10 +6,10 @@ provider "azurerm" {
   features {}
 }
 
-# data "azurerm_image" "search" {
-#   name                = var.image_name
-#   resource_group_name = var.predefined_image_rg
-# }
+data "azurerm_image" "search" {
+  name                = var.image_name
+  resource_group_name = var.resources_predefined_rg
+}
 
 resource "azurerm_resource_group" "main" {
   name     = "${var.prefix}-resources"
@@ -90,8 +90,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
   admin_password       = "password@123"
   disable_password_authentication = false
 
-
-  source_image_id = "/subscriptions/e6139af9-7952-444a-bef0-82110bcd6db5/resourceGroups/EssilorDemoNetworkRG/providers/Microsoft.Compute/images/demo-centos-image-18-09-2020"
+  source_image_id = data.azurerm_image.search.id
 
   os_disk {
     storage_account_type = "Standard_LRS"
