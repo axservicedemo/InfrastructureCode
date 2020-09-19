58ef0d3ad3320d8b4033f9178972be2d9deed5fa
axservicedemo <71280084+axservicedemo@users.noreply.github.com>
2020-09-19 14:08:06 +0530

    Added azurerm_image data

--- Calculations/main.tf
+++ Calculations/main.tf
@@ -6,23 +6,14 @@ provider "azurerm" {
   features {}
 }
 
+data "azurerm_image" "search" {
+  name                = "demo-windows-image-19-09-2020"
+  resource_group_name = "EssilorDemoNetworkRG"
+}
+
 resource "azurerm_resource_group" "main" {
   name     = "${var.prefix}-resources"
   location = var.location
-}
-
-resource "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-}
-
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_public_ip" "main" {
@@ -39,7 +30,7 @@ resource "azurerm_network_interface" "main" {
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.internal.id
+    subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.main.id
   }
@@ -52,16 +43,10 @@ resource "azurerm_windows_virtual_machine" "main" {
   size                            = "Standard_F2"
   admin_username                  = "adminuser"
   admin_password                  = "P@ssw0rd1234!"
+  source_image_id                 =  data.azurerm_image.search.id
   network_interface_ids = [
     azurerm_network_interface.main.id,
   ]
-
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2016-Datacenter"
-    version   = "latest"
-  }
 
   os_disk {
     storage_account_type = "Standard_LRS"
@@ -103,4 +88,8 @@ output "name" {
 
 output "operating_system" {
   value = "Windows"
+}
+
+output "scaleset" {
+  value = "False"
 }
