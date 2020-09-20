c3e8177e102ca5637fb08b58afb1a04be51d5adc
axservicedemo <71280084+axservicedemo@users.noreply.github.com>
2020-09-20 15:07:47 +0530

    Changed the size from F2 to F1

--- Calculations/main.tf
+++ Calculations/main.tf
@@ -6,14 +6,14 @@ provider "azurerm" {
   features {}
 }
 
-data "azurerm_image" "search" {
-  name                = "demo-windows-image-19-09-2020"
-  resource_group_name = "EssilorDemoNetworkRG"
-}
-
 resource "azurerm_resource_group" "main" {
   name     = "${var.prefix}-resources"
   location = var.location
+}
+
+data "azurerm_image" "search" {
+  name                = var.image_name
+  resource_group_name = var.resources_predefined_rg
 }
 
 resource "azurerm_public_ip" "main" {
@@ -40,7 +40,7 @@ resource "azurerm_windows_virtual_machine" "main" {
   name                            = "${var.prefix}-vm"
   resource_group_name             = azurerm_resource_group.main.name
   location                        = azurerm_resource_group.main.location
-  size                            = "Standard_F2"
+  size                            = "Standard_F1"
   admin_username                  = "adminuser"
   admin_password                  = "P@ssw0rd1234!"
   source_image_id                 =  data.azurerm_image.search.id
@@ -70,20 +70,12 @@ resource "azurerm_virtual_machine_data_disk_attachment" "example" {
   caching            = "ReadWrite"
 }
 
-output "public_ip" {
-  value = azurerm_windows_virtual_machine.main.public_ip_address
-}
-
-output "admin_username" {
-  value = azurerm_windows_virtual_machine.main.admin_username
-}
-
-output "admin_password" {
-  value = azurerm_windows_virtual_machine.main.admin_password
-}
-
 output "name" {
   value = azurerm_windows_virtual_machine.main.name
+}
+
+output "public_ip" {
+  value = azurerm_windows_virtual_machine.main.public_ip_address
 }
 
 output "operating_system" {
