13d27e5fe5c56a272895727cbaf62957335b555e
axservicedemo <71280084+axservicedemo@users.noreply.github.com>
2020-09-20 20:08:53 +0530

    Created variables.tf

---
+++ Rxlab/variables.tf
@@ -0,0 +1,28 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+}
+variable "subscription_id" {
+  description = "The Azure Secret"
+}
+variable "tenant_id" {
+  description = "The Azure Secret"
+}
+variable "client_secret" {
+  description = "The Azure Secret"
+}
+variable "client_id" {
+  description = "The Azure Secret"
+}
+variable "image_name" {
+  default = "demo-windows-image-19-09-2020"
+  description = "The Custom image Name"
+}
+variable "location" {
+  description = "The region which the resource is deployed"
+}
+variable "subnet_id" {
+  description = "The subnet ID in which the resources needs to be deployed"
+}
+variable "resources_predefined_rg" {
+  description = "The resource group where the images has be located"
+}
