871e30cd7fd9bfe2501d815b607653d7e99405ae
sindhuja <sindhuja@appranix.com>
2020-09-29 12:28:11 +0530

    Size change from B1S-B1MS

--- Calculations/main.tf
+++ Calculations/main.tf
@@ -40,7 +40,7 @@ resource "azurerm_windows_virtual_machine" "main" {
   name                            = "${var.prefix}-vm"
   resource_group_name             = azurerm_resource_group.main.name
   location                        = azurerm_resource_group.main.location
-  size                            = "Standard_B1S"
+  size                            = "Standard_B1MS"
   admin_username                  = "adminuser"
   admin_password                  = "P@ssw0rd1234!"
   source_image_id                 =  data.azurerm_image.search.id
